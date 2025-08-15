283fe6eafabf4dc0ba18b585217c8b3d0c7078b0
Jemima02 <232154@student.isfsc.be>
2025-08-15 22:57:17 +0200

    ajout de la page apropos

---
+++ apropos.php
@@ -0,0 +1,142 @@
+<?php get_header();
+/* Template Name: A propros */
+?>
+
+<main>
+    <section class="container my-5">
+        <div class="row align-items-center">
+            <div class="col-lg-6">
+                <h2 class="section-title mb-4">À Propos de Basi Unity</h2>
+                <p class="mb-4">L'entreprise a commencé avec un groupe de 5 fondatrices passionnées par les produits de
+                    beauté, qui se sont rencontrées lors d'une réunion à l'université et ont décidé de créer leur propre
+                    marque.
+                </p>
+                <p>Notre équipe s'est développée, avec maintenant plus de vingt femmes talentueuses qui travaillent
+                    ensemble
+                    pour offrir des produits exceptionnels. Notre passion pour la beauté et l'innovation continue de
+                    guider
+                    notre vision et notre mission.</p>
+            </div>
+            <div class="col-lg-6">
+                <div class="image-container">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/about/uni.jpg"
+                        alt="Unions entre plusieurs personnes" class="img-fluid rounded">
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="container mb-5">
+        <div class="row align-items-center">
+            <div class="col-lg-6 order-lg-2">
+                <h2 class="section-title mb-4">Notre Vision</h2>
+                <p class="mb-4">Chez Basi Unity, nous croyons que chaque femme mérite des produits de beauté de haute
+                    qualité
+                    qui célèbrent sa beauté naturelle. Notre vision est de créer une communauté inclusive où toutes les
+                    femmes
+                    se sentent valorisées et belles.</p>
+                <p>Nous nous engageons à développer des produits qui non seulement mettent en valeur la beauté
+                    extérieure,
+                    mais nourrissent également la confiance intérieure de chaque femme.</p>
+            </div>
+            <div class="col-lg-6 order-lg-1">
+                <div class="image-container">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/about/eyes.jpg"
+                        alt="Vision Basi Unity montré avec des yeux" class="img-fluid rounded">
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="container mb-5">
+        <div class="row">
+            <div class="col-lg-6">
+                <h2 class="section-title mb-4">Nos Objectifs</h2>
+                <p class="mb-3">• Favoriser la diversité et l'inclusion dans l'industrie de la beauté en créant des
+                    produits
+                    pour tous les types de peau et de cheveux.</p>
+                <p class="mb-3">• Utiliser des ingrédients naturels et durables pour minimiser notre impact
+                    environnemental.
+                </p>
+                <p class="mb-3">• Soutenir les communautés locales en collaborant avec des artisans et des fournisseurs
+                    locaux.</p>
+                <p class="mb-3">• Éduquer nos clients sur l'importance des soins personnels et de l'amour de soi.</p>
+                <p class="mb-3">• Devenir une référence mondiale dans l'industrie des cosmétiques naturels.</p>
+                <p class="mb-3">• Créer un environnement de travail positif et stimulant pour notre équipe.</p>
+                <p class="mb-3">• Investir dans la recherche et le développement pour innover constamment nos produits.
+                </p>
+                <p class="mb-3">• Établir des partenariats stratégiques avec d'autres marques partageant nos valeurs.
+                </p>
+                <p class="mb-3">• Organiser des événements communautaires pour renforcer les liens entre nos clients.
+                </p>
+                <p class="mb-3">• Lancer des campagnes de sensibilisation sur l'importance de l'estime de soi et de la
+                    confiance en soi.</p>
+            </div>
+            <div class="col-lg-6">
+                <div class="image-container mt-4 mt-lg-0">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/about/multiethnic.jpeg"
+                        alt="2 femmes réflechissant devant un bureau" class="img-fluid rounded">
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="container mb-5">
+        <div class="row align-items-center">
+            <div class="col-lg-6 order-lg-2">
+                <h2 class="section-title mb-4">Pourquoi rejoindre Basi Unity ?</h2>
+                <p class="mb-3">• Faire partie d'une communauté de femmes qui se soutiennent mutuellement dans leur
+                    parcours
+                    de beauté.</p>
+                <p class="mb-3">• Accéder à des produits exclusifs créés par des femmes pour des femmes.</p>
+                <p class="mb-3">• Bénéficier de conseils personnalisés adaptés à vos besoins spécifiques.</p>
+                <p class="mb-4">• Participer à des événements exclusifs et des ateliers sur la beauté et le bien-être.
+                </p>
+                <p>Chez Basi Unity, nous vous invitons à rejoindre notre famille où chaque femme est célébrée pour sa
+                    beauté
+                    unique. Ensemble, nous pouvons créer un monde où la beauté est synonyme de diversité, d'inclusion et
+                    d'authenticité.</p>
+                <p class="mb-3">Rejoignez-nous aujourd'hui et découvrez ce que signifie vraiment être belle de
+                    l'intérieur
+                    comme de l'extérieur.</p>
+            </div>
+            <div class="col-lg-6 order-lg-1">
+                <div class="image-container">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/about/faces.jpeg"
+                        alt="2 visages collées" class="img-fluid rounded">
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="container mb-5">
+        <div class="row align-items-center">
+            <div class="col-lg-6">
+                <h2 class="section-title mb-4">Qui sommes-nous ?</h2>
+                <p class="mb-4">Chez Basi Unity, nous sommes une équipe diversifiée de femmes passionnées par la beauté
+                    et
+                    déterminées à faire une différence dans l'industrie. Notre équipe comprend des chimistes, des
+                    artistes du
+                    maquillage, des spécialistes du marketing et des expertes en soins de la peau, toutes unies par une
+                    vision
+                    commune.</p>
+                <p class="mb-4">Nos fondatrices, avec leurs parcours uniques et complémentaires, apportent une richesse
+                    d'expérience et de perspectives à notre marque. Ensemble, elles ont créé une entreprise qui valorise
+                    l'authenticité, la qualité et l'innovation.</p>
+                <p>Chaque membre de notre équipe joue un rôle crucial dans notre succès collectif, et nous sommes fières
+                    de la
+                    culture de collaboration et de soutien que nous avons cultivée. Chez Basi Unity, nous ne sommes pas
+                    seulement des collègues, nous sommes une famille.</p>
+            </div>
+            <div class="col-lg-6">
+                <div class="image-container">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/about/photo.jpeg"
+                        alt="Esther et Jemima" class="img-fluid rounded">
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
+
+
+<?php get_footer(); ?>
