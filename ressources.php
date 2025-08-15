8589e789b991fedf763f123fbfe95435abf0ce45
Jemima02 <232154@student.isfsc.be>
2025-08-15 23:52:45 +0200

    ajout de la page ressources

---
+++ ressources.php
@@ -0,0 +1,86 @@
+<?php get_header();
+/* Template Name: Ressources */
+?>
+
+<main>
+    <div class="text-center py-5">
+        <h1 class="display-4 fw-light">Ressources</h1>
+    </div>
+
+    <section class="resources-section container mb-5">
+        <div class="resource-category">
+            <h2>Développement personnel et mindset entrepreneurial</h2>
+            <div class="resource-item">
+                <p>5 clés pour booster votre confiance en tant qu'entrepreneuse</p>
+                <a href="<?php echo get_template_directory_uri(); ?>/assets/ebook/EBOOK 5 CLES DE CONFIANCE.pdf"
+                    download><button class="btn custom-btn">Téléchargement</button>
+                </a>
+
+            </div>
+            <div class="resource-item">
+                <p>Gérer son temps comme une pro : Guide pour entrepreneuses occupées</p>
+                <a href="<?php echo get_template_directory_uri(); ?>/assets/ebook/GERER SON TEMPS .pdf"
+                    download><button class="btn custom-btn">Téléchargement</button>
+                </a>
+            </div>
+        </div>
+
+        <div class="resource-category">
+            <h2>Lancer son activité dans le digital</h2>
+            <div class="resource-item">
+                <p>Choisir sa niche : Trouver sa place dans le monde du digital</p>
+                <a href="<?php echo get_template_directory_uri(); ?>/assets/ebook/TROUVER SA NICHE EBOOK.pdf"
+                    download><button class="btn custom-btn">Téléchargement</button>
+                </a>
+            </div>
+        </div>
+
+        <div class="resource-category">
+            <h2>Collaboration et networking</h2>
+            <div class="resource-item">
+                <p>Collaborations réussies : Guide pour travailler avec influenceuses et freelances</p>
+                <a href="<?php echo get_template_directory_uri(); ?>/assets/ebook/COLLAB REUSSIS EBOOK.pdf"
+                    download><button class="btn custom-btn">Téléchargement</button>
+                </a>
+            </div>
+        </div>
+
+        <div class="resource-category">
+            <h2>Marketing digital et visibilité en ligne</h2>
+            <div class="resource-item">
+                <p>Créer une stratégie de contenu efficace pour votre marque</p>
+                <a href="<?php echo get_template_directory_uri(); ?>/assets/ebook/CREER UNE STRATEGIE EFFICACE.pdf"
+                    download><button class="btn custom-btn">Téléchargement</button>
+                </a>
+            </div>
+        </div>
+
+        <div class="resource-category">
+            <h2>Finances et organisation</h2>
+            <div class="resource-item">
+                <p>Créer un business plan pour votre activité digitale</p>
+                <a href="<?php echo get_template_directory_uri(); ?>/assets/ebook/BUSINESS PLAN DIGITAL .pdf"
+                    download><button class="btn custom-btn">Téléchargement</button>
+                </a>
+            </div>
+        </div>
+
+        <div class="resource-category">
+            <h2>Success stories et inspirations</h2>
+            <div class="resource-item">
+                <p>Portraits de femmes qui ont réussi dans le digital</p>
+                <button class="btn custom-btn">Téléchargement</button>
+            </div>
+        </div>
+
+        <div class="resource-category">
+            <h2>Gestion des compétences</h2>
+            <div class="resource-item">
+                <p>Comment déléguer efficacement en tant qu'entrepreneuse</p>
+                <button class="btn custom-btn">Téléchargement</button>
+            </div>
+        </div>
+    </section>
+</main>
+
+<?php get_footer(); ?>
