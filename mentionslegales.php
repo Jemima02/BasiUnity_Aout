8e72ab0c5bfb7d8131daa31f1db0ec1a06559235
Jemima02 <232154@student.isfsc.be>
2025-08-17 01:43:07 +0200

    ajout de la page mentions legales

---
+++ mentionslegales.php
@@ -0,0 +1,50 @@
+<?php get_header();
+/* Template Name: Mentions Légales */
+?>
+
+<main>
+    <div class="text-center py-5">
+        <h1 class="display-4 fw-light">Mentions légales</h1>
+    </div>
+
+    <section class="legal-section container mb-5">
+        <div class="legal-container">
+            <div class="info-row">
+                <span class="icon">→</span>
+                <p>Nom de société: Basi Unity</p>
+            </div>
+
+            <div class="info-row">
+                <span class="icon">→</span>
+                <p>Forme juridique: SRL</p>
+            </div>
+
+            <div class="info-row">
+                <span class="icon">📍</span>
+                <p>Adresse postal du siège: Avenue du Paradis 33, 1000 Bruxelles</p>
+            </div>
+
+            <div class="info-row">
+                <span class="icon">📞</span>
+                <p>Téléphone: 0488356193</p>
+            </div>
+
+            <div class="info-row">
+                <span class="icon">📧</span>
+                <p>E-mail: Basi.unity@hotmail.com</p>
+            </div>
+
+            <div class="info-row">
+                <span class="icon">→</span>
+                <p>Numéro BCE: 1234.567.890</p>
+            </div>
+
+            <div class="info-row">
+                <span class="icon">→</span>
+                <p>TVA: BE 0123.456.789</p>
+            </div>
+        </div>
+    </section>
+</main>
+
+<?php get_footer(); ?>
