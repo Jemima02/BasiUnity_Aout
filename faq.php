0ca65b6fdcd97e88a174805720b836d5bbb6e2d8
Jemima02 <232154@student.isfsc.be>
2025-08-16 20:23:14 +0200

    ajout de la page faq

---
+++ faq.php
@@ -0,0 +1,60 @@
+<?php get_header();
+/* Template Name: faq */
+?>
+
+<main>
+    <div class="text-center py-5">
+        <h1 class="display-4 fw-light">FAQ</h1>
+    </div>
+
+    <section class="faq-section container mb-5">
+        <div class="faq-container">
+            <div class="faq-item">
+                <h2>Qu'est-ce que c'est Basi Unity ?</h2>
+                <p>Basi Unity est une plateforme d'entraide dédiée aux femmes entrepreneuses dans le domaine du
+                    digital. Nous proposons des ressources, des guides, des conseils et un espace communautaire pour
+                    vous aider à développer et à faire prospérer vos projets numériques.</p>
+            </div>
+
+            <div class="faq-item">
+                <h2>À qui s'adresse cette plateforme ?</h2>
+                <p>Elle est conçue pour toutes les femmes entrepreneuses, qu'elles soient débutantes ou
+                    expérimentées, qui souhaitent évoluer dans le monde digital et bénéficier d'un réseau qui les
+                    soutient.</p>
+            </div>
+
+            <div class="faq-item">
+                <h2>Comment puis-je m'inscrire sur la plateforme ?</h2>
+                <p>L'inscription se fait en ligne en remplissant un formulaire disponible sur notre site. Il suffit
+                    de fournir vos informations de base et une description de votre activité.</p>
+            </div>
+
+            <div class="faq-item">
+                <h2>Est-ce que l'inscription est gratuite ?</h2>
+                <p>Nous proposons une inscription gratuite avec un accès aux différents profils de base et
+                    ressources limitées.</p>
+            </div>
+
+            <div class="faq-item">
+                <h2>Les échanges au sein de la communauté sont-ils modérés ?</h2>
+                <p>Oui, nous veillons à ce que les interactions restent respectueuses et professionnelles. Une
+                    équipe dédiée modère les discussions pour garantir un environnement positif.</p>
+            </div>
+
+            <div class="faq-item">
+                <h2>Puis-je obtenir de l'aide si j'ai des questions spécifiques ?</h2>
+                <p>Oui, notre équipe de support est disponible pour répondre à vos questions via le chat en direct
+                    ou par email.</p>
+            </div>
+
+            <div class="faq-item">
+                <h2>Que faire si j'ai oublié mon mot de passe ?</h2>
+                <p>Cliquez sur "Mot de passe oublié" sur la page de connexion. Un lien de réinitialisation vous sera
+                    envoyé par email.</p>
+            </div>
+        </div>
+    </section>
+</main>
+
+
+<?php get_footer(); ?>
